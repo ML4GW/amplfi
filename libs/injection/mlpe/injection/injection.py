3a4dfdc0373e12e82fd1f7a1d722fc3b3076ea6a
ethan.marx <ethan.marx@ligo.org>
2022-10-07 08:19:45 -0700

    injection rewrite

--- libs/injection/mlpe/injection/injection.py
+++ libs/injection/mlpe/injection/injection.py
@@ -1,148 +1,81 @@
-from typing import Dict
+from typing import Callable, Dict, List, Optional
 
-import bilby
 import numpy as np
+from bilby.gw.waveform_generator import WaveformGenerator
 
 
 def generate_gw(
-    sample_params: Dict[str, np.ndarray],
-    domain: str = "time",
-    waveform_generator=bilby.gw.waveform_generator.WaveformGenerator,
+    sample_params: Dict[List, str],
+    sample_rate: float,
+    waveform_duration: float,
+    waveform: Callable,
+    waveform_arguments: Optional[Dict] = None,
+    parameter_conversion: Optional[Callable] = None,
 ):
-    """Generate raw gravitational-wave signals, pre-interferometer projection.
+    """
+    Generate raw frequency domain gravitational-wave signals
+    pre-interferometer projection.
 
     Args:
-        sample_params: dictionary of GW parameters produced by
-            the bilby.PriorDict sample function
-        domain: domain in which to generate data ("time" or "frequency")
-        waveform_generator:
-            a fully initialized bilby.gw.waveform_generator.WaveformGenerator
+        sample_params:
+            Dictionary of GW parameters where key is the parameter name
+            and value is a list of the parameters
+        sample_rate:
+            Rate at which to sample time series
+        waveform_duration:
+            Duration of waveform
+        waveform:
+            Callable whose first argument is a frequency array
+            and returns returns frequency domain
+            hplus and hcross polarizations at the specified frequencies
+            for a given set of parameters that are the following arguments
+        waveform_arguments:
+            A dictionary of fixed keyword arguments to pass to
+            frequency_domain_source_model via bilby waveform generator
+        parameter_conversion:
+            Callable to convert from sampled parameters
+            to parameters of the waveform generator.
+            Default value is the identity,
+            i.e. it leaves the parameters unaffected.
+            Typically used when generating
+            CBC waveforms via lal_binary_black_hole
 
     Returns:
         An (n_samples, 2, waveform_size) array, containing both polarizations
-        for each of the desired number of samples. The first polarization is
-        always plus and the second is always cross
+        for each of the desired number of samples.
+        The waveforms are shifted such that
+        the coalescence time lies at the center of the sample
     """
 
-    # reformat sample parameters
     sample_params = [
         dict(zip(sample_params, col)) for col in zip(*sample_params.values())
     ]
+
     n_samples = len(sample_params)
 
-    # extract signal properties from waveform generator
-    sample_rate = waveform_generator.sampling_frequency
-    waveform_duration = waveform_generator.duration
+    waveform_generator = WaveformGenerator(
+        duration=waveform_duration,
+        sampling_frequency=sample_rate,
+        frequency_domain_source_model=waveform,
+        waveform_arguments=waveform_arguments,
+        parameter_conversion=parameter_conversion,
+    )
 
-    # infer waveform size depending on
-    # domain
-    if domain == "time":
-        waveform_size = int(sample_rate * waveform_duration)
+    waveform_size = int(sample_rate * waveform_duration)
 
-        signals = np.zeros((n_samples, 2, waveform_size))
-
-    elif domain == "frequency":
-        fmax = sample_rate / 2
-        df = 1 / waveform_duration
-        waveform_size = int(fmax / df) + 1
-        signals = np.zeros((n_samples, 2, waveform_size), dtype=np.complex)
+    num_pols = 2
+    signals = np.zeros((n_samples, num_pols, waveform_size))
 
     for i, p in enumerate(sample_params):
+        polarizations = waveform_generator.time_domain_strain(p)
+        polarization_names = sorted(polarizations.keys())
+        polarizations = np.stack(
+            [polarizations[p] for p in polarization_names]
+        )
 
-        # generate data based on domain requested
-        if domain == "time":
-            polarizations = waveform_generator.time_domain_strain(p)
-        elif domain == "frequency":
-            polarizations = waveform_generator.frequency_domain_strain(p)
-
-            polarization_names = sorted(polarizations.keys())
-
-            polarizations = np.stack(
-                [polarizations[p] for p in polarization_names]
-            )
-
-            signals[i] = polarizations
-
-    return signals
-
-
-def project_raw_gw(
-    raw_waveforms: np.ndarray,
-    sample_params: Dict[str, np.ndarray],
-    duration: float,
-    ifo: str,
-    domain: str,
-):
-    """Project a raw gravitational wave onto an interferometer
-
-    Args:
-        raw_waveforms: an array of size (n_samples, 2, waveform_size)
-        sample_params: parameters produced by bilby prior sample function.
-            must include ra, dec geocen_time and psi
-        duration: length of the waveform
-        ifo: which ifo projecting data onto to
-        domain: domain of raw_waveform data (time or frequency)
-
-    Returns:
-        An (n_samples, waveform_size) array containing the GW signals as they
-        would appear in the given interferometer with the given set of sample
-        parameters. If get_snr=True, also returns a list of the SNR associated
-        with each signal
-    """
-
-    # format polarizations
-    polarizations = {
-        "plus": raw_waveforms[:, 0, :],
-        "cross": raw_waveforms[:, 1, :],
-    }
-
-    # format sample params
-    sample_params = [
-        dict(zip(sample_params, col)) for col in zip(*sample_params.values())
-    ]
-    n_sample = len(sample_params)
-
-    # infer waveform parameters from passed array
-    waveform_size = raw_waveforms.shape[-1]
-
-    # initiate signal array
-    signals = np.zeros((n_sample, waveform_size))
-
-    ifo = bilby.gw.detector.get_empty_interferometer(ifo)
-    for i, p in enumerate(sample_params):
-
-        # unpack sky loc params
-        ra = p["ra"]
-        dec = p["dec"]
-        geocent_time = p["geocent_time"]
-        psi = p["psi"]
-
-        # calculate dt shift for this ifo;
-        # first shift signal to center,
-        # then apply shift from geocenter
-        dt = duration / 2.0
-        dt += ifo.time_delay_from_geocenter(ra, dec, geocent_time)
-
-        # generate signal in ifo
-        signal = np.zeros(waveform_size)
-        for mode, polarization in polarizations.items():
-
-            # get ifo response
-            response = ifo.antenna_response(ra, dec, geocent_time, psi, mode)
-            signal += response * polarization[i]
-
-        # apply shift
-        if domain == "time":
-            sample_rate = waveform_size / duration
-            signal = np.roll(signal, int(np.round(dt * sample_rate)))
-
-        elif domain == "frequency":
-            df = 1 / duration
-            fmax = waveform_size * df
-            frequencies = np.linspace(0, fmax, df)
-            # shift in frequency domain is phase shift
-            signal = signal * np.exp(-1j * 2 * np.pi * dt * frequencies)
-
-        signals[i] = signal
+        # center so that coalescence time is middle sample
+        dt = waveform_duration / 2
+        polarizations = np.roll(polarizations, int(dt * sample_rate), axis=-1)
+        signals[i] = polarizations
 
     return signals
--- libs/injection/tests/test_injection.py
+++ libs/injection/tests/test_injection.py
@@ -6,13 +6,13 @@ import bilby
 import mlpe.injection
 import mlpe.injection.waveforms as waveforms
 import pytest
-from bilby.gw.source import lal_binary_black_hole
+from bilby.gw.conversion import convert_to_lal_binary_black_hole_parameters
 
 TEST_DIR = Path(__file__).resolve().parent
 
 
-@pytest.fixture(params=["time", "frequency"])
-def domain(request):
+@pytest.fixture(params=[10, 100])
+def n_samples(request):
     return request.param
 
 
@@ -26,14 +26,13 @@ def sample_rate(request):
     return request.param
 
 
-@pytest.fixture(params=[10, 32])
-def flow(request):
-    return request.param
-
-
-@pytest.fixture(params=["IMRPhenomPv2", "TaylorF2"])
-def approximant(request):
-    return request.param
+@pytest.fixture(
+    params=[
+        "priors/sine_gaussian.prior",
+    ]
+)
+def sg_prior_file(request):
+    return str(Path(__file__).resolve().parent / request.param)
 
 
 @pytest.fixture(params=["priors/nonspin_BBH.prior"])
@@ -41,91 +40,48 @@ def cbc_prior_file(request):
     return str(Path(__file__).resolve().parent / request.param)
 
 
-@pytest.fixture(params=["priors/sine_gaussian.prior"])
-def sg_prior_file(request):
-    return str(Path(__file__).resolve().parent / request.param)
+def test_generate_gw_sg(
+    sg_prior_file, waveform_duration, sample_rate, n_samples
+):
 
-
-@pytest.fixture
-def cbc_generator(flow, approximant, waveform_duration, sample_rate):
-    waveform_generator = bilby.gw.waveform_generator.WaveformGenerator(
-        duration=waveform_duration,
-        sampling_frequency=sample_rate,
-        frequency_domain_source_model=lal_binary_black_hole,
-        waveform_arguments={
-            "waveform_approximant": approximant,
-            "reference_frequency": 50,
-            "minimum_frequency": flow,
-        },
-    )
-    return waveform_generator
-
-
-@pytest.fixture
-def sg_generator(waveform_duration, sample_rate):
-    waveform_generator = bilby.gw.waveform_generator.WaveformGenerator(
-        duration=waveform_duration,
-        sampling_frequency=sample_rate,
-        frequency_domain_source_model=waveforms.sine_gaussian_frequency,
-    )
-
-    return waveform_generator
-
-
-def test_generate_gw(cbc_prior_file, domain, cbc_generator):
-    """Test generate_gw using supplied waveform generator, or
-    initializing generator
-    """
-
-    duration = cbc_generator.duration
-    sample_rate = cbc_generator.sampling_frequency
-
-    sample_params = bilby.gw.prior.BBHPriorDict(cbc_prior_file).sample(10)
+    # first test custom sine gaussian
+    n_pols = 2
+    waveform_size = sample_rate * waveform_duration
+    sample_params = bilby.gw.prior.PriorDict(sg_prior_file).sample(n_samples)
 
     signals = mlpe.injection.generate_gw(
         sample_params,
-        domain,
-        cbc_generator,
+        sample_rate,
+        waveform_duration,
+        waveform=waveforms.sine_gaussian_frequency,
     )
 
-    if domain == "time":
-        waveform_size = duration * sample_rate
-
-    elif domain == "frequency":
-        df = 1 / duration
-        fmax = sample_rate / 2
-        waveform_size = int(fmax / df) + 1
-
-    expected_signal_shape = (10, 2, waveform_size)
+    expected_signal_shape = (n_samples, n_pols, waveform_size)
     assert signals.shape == expected_signal_shape
 
 
-def test_generate_gw_sg(sg_prior_file, domain, sg_generator):
-    """Test generate_gw using supplied waveform generator, or
-    initializing generator
-    """
+def test_generate_gw_cbc(
+    cbc_prior_file, waveform_duration, sample_rate, n_samples
+):
 
-    duration = sg_generator.duration
-    sample_rate = sg_generator.sampling_frequency
+    # first test custom sine gaussian
+    n_pols = 2
+    waveform_size = sample_rate * waveform_duration
+    sample_params = bilby.gw.prior.PriorDict(cbc_prior_file).sample(n_samples)
 
-    sample_params = bilby.gw.prior.PriorDict(sg_prior_file).sample(10)
-
+    waveform_arguments = dict(
+        reference_frequency=20,
+        minimum_frequency=30,
+        waveform_approximant="IMRPhenomPv2",
+    )
     signals = mlpe.injection.generate_gw(
         sample_params,
-        domain,
-        sg_generator,
+        sample_rate,
+        waveform_duration,
+        waveform=bilby.gw.source.lal_binary_black_hole,
+        waveform_arguments=waveform_arguments,
+        parameter_conversion=convert_to_lal_binary_black_hole_parameters,
     )
 
-    if domain == "time":
-        waveform_size = duration * sample_rate
-
-    elif domain == "frequency":
-        df = 1 / duration
-        fmax = sample_rate / 2
-        waveform_size = int(fmax / df) + 1
-
-    expected_signal_shape = (10, 2, waveform_size)
+    expected_signal_shape = (n_samples, n_pols, waveform_size)
     assert signals.shape == expected_signal_shape
-
-
-# TODO: projection tests
