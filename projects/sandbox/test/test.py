7b4fa93b497135774e1017a68f9a8a509fd74fce
ethan.marx <ethan.marx@ligo.org>
2023-03-07 10:45:28 -0800

    rename sample to test

--- projects/sandbox/test/test.py
+++ projects/sandbox/test/test.py
@@ -1,5 +1,4 @@
 import logging
-import random
 from pathlib import Path
 from time import time
 from typing import List
@@ -66,11 +65,26 @@ def _cast_as_bilby_result(samples, truth, inference_params, priors):
     )
 
 
+def generate_corner_plots(
+    results: List[bilby.core.result.Result], writedir: Path
+):
+
+    writedir.mkdir(exist_ok=True, parents=True)
+    for i, result in enumerate(results):
+        filename = writedir / f"corner_{i}.png"
+        result.plot_corner(
+            save=True,
+            filename=filename,
+            levels=(0.5, 0.9),
+        )
+
+
 @architecturize
 def main(
     architecture: callable,
     model_state_path: Path,
     ifos: List[str],
+    channel: str,
     sample_rate: float,
     kernel_length: float,
     fduration: float,
@@ -135,7 +149,7 @@ def main(
     results = []
     descaled_results = []
     total_sampling_time = 0.0
-    num_plotted = 0  # corner plots for diagnostic analysis
+
     for signal, param in test_dataloader:
         signal = signal.to(device)
         param = param.to(device)
@@ -169,34 +183,12 @@ def main(
         logging.debug("Time taken to sample: %.2f" % (_time))
         total_sampling_time += _time
 
-        # generate diagnostic posteriors
-        corner_plot_dir = writedir / "corner"
-        corner_plot_dir.mkdir(exist_ok=True, parents=True)
-        if random.random() > 0.5 and num_plotted < num_plot_corner:
-            scaled_corner_plot_filename = (
-                corner_plot_dir / f"{num_plotted}_scaled_corner.png"
-            )
-            res.plot_corner(
-                save=True,
-                filename=scaled_corner_plot_filename,
-                levels=(0.5, 0.9),
-            )
-
-            descaled_corner_plot_filename = (
-                corner_plot_dir / f"{num_plotted}_descaled_corner.png"
-            )
-            descaled_res.plot_corner(
-                save=True,
-                filename=descaled_corner_plot_filename,
-                levels=(0.5, 0.9),
-            )
-            num_plotted += 1
-
     logging.info(
         "Total/Avg. samlping time: {:.1f}/{:.2f}(s)".format(
             total_sampling_time, total_sampling_time / num_samples_draw
         )
     )
+
     logging.info("Making pp-plot")
     pp_plot_dir = writedir / "pp_plots"
     pp_plot_scaled_filename = pp_plot_dir / "pp-plot-test-set-scaled.png"
@@ -215,4 +207,87 @@ def main(
         filename=pp_plot_filename,
         keys=inference_params,
     )
-    logging.info("Plot saved in %s" % (pp_plot_filename))
+    logging.info("PP Plots saved in %s" % (pp_plot_dir))
+
+    # TODO: this project is getting long. Should we split it up?
+    # What's the best way to do this?
+
+    # now analyze the injections we've analyzed with bilby.
+    logging.info("Comparing with bilby results")
+
+    # load in the bilby injection parameters
+    params = []
+    with h5py.File(datadir / "bilby" / "bilby_injection_parameters.hdf5") as f:
+        times = f["geocent_times"][:]
+        for param in inference_params:
+            values = f[param][:]
+            # take logarithm since hrss
+            # spans large magnitude range
+            if param == "hrss":
+                values = np.log10(values)
+            params.append(values)
+
+        params = np.vstack(params).T
+
+    # load in the timeseries data and crop around the injection times
+    timeseries = []
+    injections = []
+    for ifo in ifos:
+        with h5py.File(
+            datadir / "bilby" / f"{ifo}_bilby_injections.hdf5"
+        ) as f:
+            timeseries.append(f["{ifo}:{channel}"][:])
+    timeseries = np.stack(timeseries)
+
+    start_indices = int((times - kernel_length) * sample_rate)
+    end_indices = start_indices + int(kernel_length * sample_rate)
+
+    for start, stop in zip(start_indices, end_indices):
+        injections.append(timeseries[start:stop])
+
+    injections = np.stack(injections)
+
+    test_data = torch.from_numpy(injections).to(torch.float32)
+    test_params = torch.from_numpy(params).to(torch.float32)
+
+    test_dataset = torch.utils.data.TensorDataset(test_data, test_params)
+    test_dataloader = torch.utils.data.DataLoader(
+        test_dataset,
+        pin_memory=False if device == "cpu" else True,
+        batch_size=1,
+        pin_memory_device=device,
+    )
+
+    for signal, param in test_dataloader:
+        signal = signal.to(device)
+        param = param.to(device)
+        strain, scaled_param = preprocessor(signal, param)
+
+        _time = time()
+        with torch.no_grad():
+            samples = flow_obj.flow.sample(num_samples_draw, context=strain)
+            descaled_samples = preprocessor.scaler(
+                samples[0].transpose(1, 0), reverse=True
+            )
+            descaled_samples = descaled_samples.unsqueeze(0).transpose(2, 1)
+        _time = time() - _time
+
+        descaled_res = _cast_as_bilby_result(
+            descaled_samples.cpu().numpy(),
+            param.cpu().numpy(),
+            inference_params,
+            priors,
+        )
+        descaled_results.append(descaled_res)
+
+        res = _cast_as_bilby_result(
+            samples.cpu().numpy(),
+            scaled_param.cpu().numpy(),
+            inference_params,
+            priors,
+        )
+        results.append(res)
+
+    # generate corner plots for sanity checking
+    generate_corner_plots(results, writedir / "corner" / "scaled")
+    generate_corner_plots(descaled_results, writedir / "corner" / "descaled")
