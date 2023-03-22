8240800a6301eb4ee887e8c71e5aaf125e6d5fee
ethan.marx <ethan.marx@ligo.org>
2023-03-22 11:46:42 -0700

    update env

--- projects/sandbox/data_generation/data_generation/utils.py
+++ projects/sandbox/data_generation/data_generation/utils.py
@@ -1,11 +1,10 @@
-from typing import Iterable, Tuple, Union
+from typing import Iterable, Tuple
 
 import gwdatafind
 import lal
 import lalsimulation
 import numpy as np
 import torch
-from astropy.time import Time
 from gwpy.timeseries import TimeSeries, TimeSeriesDict
 
 from ml4gw.utils.slicing import slice_kernels
@@ -186,44 +185,3 @@ def inject_into_background(
     data[idx] += waveforms
 
     return data
-
-
-def ra_from_phi(phi: Union[np.ndarray, float], gpstime: float) -> float:
-    """
-    Calculate the right ascension of a source given its relative
-    azimuthal angle and the geocentric time of the observation
-    """
-
-    # get the sidereal time at the observation time
-    t = Time(gpstime, format="gps", scale="utc")
-    gmst = t.sidereal_time("mean", "greenwich").to("rad")
-
-    if isinstance(phi, float):
-        phi = np.array([phi])
-
-    # convert phi from range [-pi, pi] to [0, 2pi]
-    mask = phi < 0
-    phi[mask] += 2 * np.pi
-
-    return (phi + gmst) % (2 * np.pi)
-
-
-def phi_from_ra(ra: Union[np.ndarray, float], gpstime: float) -> float:
-
-    # get the sidereal time at the observation time
-    t = Time(gpstime, format="gps", scale="utc")
-    gmst = t.sidereal_time("mean", "greenwich").to("rad")
-
-    if isinstance(ra, float):
-        ra = np.array([ra])
-
-    # calculate the relative azimuthal angle in the range [0, 2pi]
-    phi = ra - gmst
-    mask = phi < 0
-    phi[mask] += 2 * np.pi
-
-    # convert phi from range [0, 2pi] to [-pi, pi]
-    mask = phi > np.pi
-    phi[mask] -= 2 * np.pi
-
-    return phi
