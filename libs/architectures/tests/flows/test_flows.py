52423b62ac5e00cf21ef81994a31913d85da3149
Ethan Jacob Marx <ethan.marx@ligo.org>
2023-04-08 10:07:44 -0700

    get arch tests working

--- libs/architectures/tests/flows/test_flows.py
+++ libs/architectures/tests/flows/test_flows.py
@@ -1,6 +1,7 @@
 import pytest
 import torch
 
+from mlpe.architectures.embeddings import ResNet
 from mlpe.architectures.flows import CouplingFlow, MaskedAutoRegressiveFlow
 
 
@@ -20,16 +21,17 @@ def n_ifos(request):
 
 
 @pytest.fixture(params=[1, 2, 4])
-def num_flow_steps(request):
+def num_transforms(request):
     return request.param
 
 
-def test_coupling_flow(param_dim, strain_dim, n_ifos, num_flow_steps):
+def test_coupling_flow(param_dim, strain_dim, n_ifos, num_transforms):
     data = torch.randn((100, param_dim))
     strain = torch.randn((100, n_ifos, strain_dim))
+    embedding = ResNet((n_ifos, None), layers=[1, 1], context_dim=10)
 
     coupling_flow = CouplingFlow(
-        (param_dim, n_ifos, strain_dim), num_flow_steps
+        (param_dim, n_ifos, strain_dim), embedding, num_transforms
     )
 
     flow = coupling_flow.flow
@@ -39,13 +41,14 @@ def test_coupling_flow(param_dim, strain_dim, n_ifos, num_flow_steps):
 
 
 def test_masked_autoregressive_flow(
-    param_dim, strain_dim, n_ifos, num_flow_steps
+    param_dim, strain_dim, n_ifos, num_transforms
 ):
+
     data = torch.randn((100, param_dim))
     strain = torch.randn((100, n_ifos, strain_dim))
-
+    embedding = ResNet((n_ifos, None), layers=[1, 1], context_dim=10)
     maf = MaskedAutoRegressiveFlow(
-        (param_dim, n_ifos, strain_dim), num_flow_steps
+        (param_dim, n_ifos, strain_dim), embedding, num_transforms
     )
     flow = maf.flow
     log_likelihoods = flow.log_prob(data, context=strain)
