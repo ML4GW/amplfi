17ac784881552c69e08e4a5f494d39e5ebc18582
Ethan Jacob Marx <ethan.marx@ligo.org>
2023-04-08 09:55:27 -0700

    remove comment from arch library

--- libs/architectures/mlpe/architectures/__init__.py
+++ libs/architectures/mlpe/architectures/__init__.py
@@ -9,7 +9,6 @@ from mlpe.architectures.embeddings import (
 from mlpe.architectures.flows import CouplingFlow, MaskedAutoRegressiveFlow
 
 
-# see the comment in the `embeddings` dict below for an explanation of this
 def _wrap_flow(arch):
     def func(*args, **kwargs):
         def f(shape, embedding):
@@ -21,25 +20,6 @@ def _wrap_flow(arch):
     params = list(params.values())[2:]
     func.__signature__ = inspect.Signature(params)
     return func
-
-
-# this is a decorator that takes an embedding
-# and returns a function `func` whose
-# arguments are the same arguments as the embedding,
-# but with the first argument
-# (the "shape", i.e. number of ifos and the parameter dimension) removed.
-
-# This allows us to specify the embedding
-# as a callable in a function signature,
-# and expose the embedding's arguments at the command line.
-
-# When typeo parses the config file, it will call the function `func` with the
-# arguments specified in the config file. `func` returns another function `f`,
-# which has the embeddings arguments passed to it.
-
-# This function `f` will then be called with the shape as the first argument
-# and will instantiate the embedding.
-# All of embeddings arguments are automatically passed to `f`.
 
 
 def _wrap_embedding(embedding):
