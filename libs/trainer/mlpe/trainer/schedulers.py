dec2fe2c0cbefd73481571f1177b5ea65fba653b
Deep Chatterjee <deep.chatterjee@ligo.org>
2023-06-12 18:22:03 -0400

    add sequential scheduler

--- libs/trainer/mlpe/trainer/schedulers.py
+++ libs/trainer/mlpe/trainer/schedulers.py
@@ -33,6 +33,16 @@ class OneCycleLR(torch.optim.lr_scheduler.OneCycleLR):
         )
 
 
+class ConstantLR(torch.optim.lr_scheduler.ConstantLR):
+    def __init__(self, optimizer: torch.optim.Optimizer, total_iters=5):
+        super().__init__(optimizer, total_iters=total_iters)
+
+
+class ExponentialLR(torch.optim.lr_scheduler.ExponentialLR):
+    def __init__(self, optimizer: torch.optim.Optimizer, gamma=0.99):
+        super().__init__(optimizer, gamma=gamma)
+
+
 class CosineAnnealingLR(torch.optim.lr_scheduler.CosineAnnealingLR):
     def __init__(
         self,
@@ -43,7 +53,49 @@ class CosineAnnealingLR(torch.optim.lr_scheduler.CosineAnnealingLR):
         super().__init__(optimizer, T_max=T_max, eta_min=eta_min)
 
 
+class SequentialLR(torch.optim.lr_scheduler.SequentialLR):
+    def __init__(
+        self,
+        optimizer: torch.optim.Optimizer,
+        schedulers: list,
+        scheduler_kwargs: list,
+        milestones: list,
+    ):
+        """Learning rate schedulers
+
+        Args:
+            optimizer:
+                Learning rate optimizer object
+            schedulers:
+                List of string names for schedulers. Assumed to
+                be a valid torch scheduler. E.g.
+                `['ConstantLR', 'CosineAnnealingLR']` initialized
+                with `optimizer`.
+            scheduler_kwargs:
+                List of keyword arguments passed to ``schedulers``.
+                Supply as strings.
+                E.g. `["dict(total_iters=5)", "dict(T_max=100)"]`
+            milestones:
+                List of milestones passed to
+                ``torch.optim.lr_scheduler.SequentialLR``.
+        """
+        _schedulers = [
+            getattr(torch.optim.lr_scheduler, str_name)(
+                optimizer, **eval(kwargs)
+            )
+            for str_name, kwargs in zip(schedulers, scheduler_kwargs)
+        ]
+        super().__init__(
+            optimizer,
+            schedulers=_schedulers,
+            milestones=[eval(_) for _ in milestones],
+        )
+
+
 schedulers = {
     "onecycle": _wrap_callable(OneCycleLR),
+    "constant": _wrap_callable(ConstantLR),
+    "exponential": _wrap_callable(ExponentialLR),
     "cosine": _wrap_callable(CosineAnnealingLR),
+    "sequential": _wrap_callable(SequentialLR),
 }
