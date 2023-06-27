72e982d6e86073427331530d35e8bb226d39db4c
Ethan Jacob Marx <ethan.marx@ligo.org>
2023-06-27 05:43:39 -0700

    change run script to sequentail

--- projects/sandbox/train/run.sh
+++ projects/sandbox/train/run.sh
@@ -1,1 +1,1 @@
-pinto run train --typeo ../pyproject.toml script=train flow=maf embedding=resnet optimizer=adam scheduler=
+pinto run train --typeo ../pyproject.toml script=train flow=maf embedding=resnet optimizer=adam scheduler=sequential
