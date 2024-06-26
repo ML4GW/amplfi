bf13a487161a535445d5b1a36913cb3d759f54a9
Ethan Jacob Marx <ethan.marx@ligo.org>
2024-06-26 06:09:11 -0700

    add more documentation

--- amplfi/law/amplfi/law/base.py
+++ amplfi/law/amplfi/law/base.py
@@ -4,12 +4,25 @@ from amplfi.law.paths import paths
 
 
 class AmplfiDataTaskMixin:
+    """
+    Mixin class for appending additional apptainer
+    and/or condor environment variable to `mldatafind.law.DataTask`
+
+    This class should be inherited
+    alongside a subclass of `mldatafind.law.DataTask`
+    """
+
     def __init__(self, *args, **kwargs):
+        # calls `mldatafind.law.DataTask` constructor
         super().__init__(*args, **kwargs)
         self.image = paths().container_root / "data.sif"
 
-    def sandbox_env(self, env):
-        # get `mldatafind.law.DataTask` environment variables
+    def sandbox_env(self, env: dict[str, str]) -> dict[str, str]:
+        """
+        Append amplfi specific environment variables to the sandbox environment
+        """
+        # get environment variables defined by
+        # `mldatafind.law.DataTask` parent class
         env = super().sandbox_env(env)
 
         # append amplfi specific environment variables
@@ -19,7 +32,10 @@ class AmplfiDataTaskMixin:
         return env
 
     def build_environment(self) -> str:
-        # get `mldatafind.law.condor.LDGCondorWorkflow` environment
+        """
+        Append amplfi specific environment variables to the condor submit file
+        """
+        # get `mldatafind.law.condor.LDGCondorWorkflow` environment variables
         environment = super().build_environment()
 
         # forward any env variables that start with AMPLFI_
