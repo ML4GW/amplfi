167fc942926c9d15b7e10fd0de22849b0de5daf7
Ethan Jacob Marx <ethan.marx@ligo.org>
2024-08-09 12:32:25 -0700

    rm run file

--- amplfi/law/run.sh
+++
@@ -1,4 +0,0 @@
-export AMPLFI_DATADIR=~/amplfi/my-first-run/data/
-export AMPLFI_CONDORDIR=~/amplfi/my-first-run/condor
-
-LAW_CONFIG_FILE=./config.cfg poetry run law run amplfi.law.DataGeneration --workers 2 --dev
