89ac63d5984e78a8272a3919a99046f501d584bb
Ethan Jacob Marx <ethan.marx@ligo.org>
2024-09-20 09:25:50 -0700

    add optional batch norm to flows

--- amplfi/architectures/amplfi/architectures/flows/base.py
+++ amplfi/architectures/amplfi/architectures/flows/base.py
@@ -3,8 +3,9 @@ from pathlib import Path
 from typing import Callable, Optional
 
 import torch
-from pyro.distributions import ConditionalTransformedDistribution, transforms
+from pyro.distributions import ConditionalTransformedDistribution
 from pyro.distributions.conditional import ConditionalComposeTransformModule
+from pyro.distributions.transforms import BatchNorm, Transform
 from pyro.nn import PyroModule
 
 
@@ -16,12 +17,14 @@ class FlowArchitecture(PyroModule):
         embedding_net: torch.nn.Module,
         embedding_weights: Optional[Path] = None,
         freeze_embedding: bool = False,
+        use_batch_norm: bool = True,
     ):
 
         super().__init__()
         self.num_params = num_params
         self.context_dim = context_dim
         self.embedding_net = embedding_net
+        self.use_batch_norm = use_batch_norm
 
         if freeze_embedding:
             self.embedding_context = torch.no_grad
@@ -31,17 +34,19 @@ class FlowArchitecture(PyroModule):
         if embedding_weights is not None:
             self.embedding_net.load_state_dict(torch.load(embedding_weights))
 
-    def transform_block(
-        self, *args, **kwargs
-    ) -> Callable[[int], transforms.Transform]:
+    def transform_block(self, *args, **kwargs) -> Callable[[int], Transform]:
         raise NotImplementedError
 
     def distribution(self) -> torch.distributions.Distribution:
         raise NotImplementedError
 
     def build_transforms(self) -> ConditionalComposeTransformModule:
-        """Sets the ``transforms`` attribute"""
-        raise NotImplementedError
+        transforms = []
+        for _ in range(self.num_transforms):
+            transforms.extend([self.transform_block()])
+            if self.use_batch_norm:
+                transforms.extend([BatchNorm(self.num_params)])
+        return ConditionalComposeTransformModule(transforms)
 
     def flow(self) -> ConditionalTransformedDistribution:
         return ConditionalTransformedDistribution(
