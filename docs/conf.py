bd20f59641acf410bc48baa7d4f255422b3643ba
Ethan Jacob Marx <ethan.marx@ligo.org>
2024-09-29 23:49:00 -0700

    add myst parser extensions

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,21 @@ extensions = [
     "myst_parser",
 ]
 
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
 
 source_suffix = [".rst", ".md"]
 
