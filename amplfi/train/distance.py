5ec102081681ccc2d4b9065bad71ab54dd041027
Ravi Kumar <ravioli1369@gmail.com>
2025-01-31 12:08:26 +0000

    move things to ml4gw

--- amplfi/train/distance.py
+++ amplfi/train/distance.py
@@ -2,45 +2,7 @@
 
 import torch
 from ml4gw.constants import PI
-
-
-def root_scalar(f, x0, args=(), fprime=None, maxiter=100, xtol=1e-6):
-    """
-    Find a root of a scalar function.
-
-    Args:
-        f (callable): The function whose root is to be found.
-        x0 (float): Initial guess.
-        args (tuple, optional): Extra arguments passed to the objective
-        function `f` and its derivative(s).
-        fprime (callable, optional): The derivative of the function.
-        xtol (float, optional): The tolerance for the root.
-        maxiter (int, optional): The maximum number of iterations.
-
-    Returns:
-        dict: A dictionary containing the root, and whether the optimization
-        was successful.
-    """
-    if x0 is None:
-        raise ValueError("x0 must be provided")
-    res = {"converged": False, "root": None}
-    for _ in range(maxiter):
-        fx = f(x0, *args)
-        if fprime is not None:
-            fpx = fprime(x0, *args)
-        else:
-            fpx = (f(x0 + xtol, *args) - f(x0 - xtol, *args)) / (2 * xtol)
-        if abs(fpx) < torch.finfo(torch.float).eps:
-            res["root"] = x0
-            res["converged"] = True
-            return res
-        x1 = x0 - fx / fpx
-        if abs(x1 - x0) < xtol:
-            res["root"] = x1
-            res["converged"] = True
-            return res
-        x0 = x1
-    return res
+from ml4gw.optimize import root_scalar
 
 
 def P(x):
--- amplfi/train/testing.py
+++ amplfi/train/testing.py
@@ -3,80 +3,13 @@ from pathlib import Path
 import bilby
 import healpy as hp
 import matplotlib.pyplot as plt
+import ml4gw.utils.healpix as mlhp
 import torch
 from astropy import io, table
 from astropy import units as u
 from ml4gw.constants import PI
 
 from . import distance
-
-max_nside = 1 << 29
-
-
-def nest2uniq(nside, ipix):
-    return 4 * nside * nside + ipix
-
-
-def nside2npix(nside):
-    return 12 * nside * nside
-
-
-def nside2pixarea(nside, degrees=True):
-    pixarea = 4 * PI / nside2npix(nside)
-
-    if degrees:
-        pixarea = pixarea * (180.0 / PI) ** 2
-
-    return pixarea
-
-
-def isnsideok(nside, nest=False):
-    if hasattr(nside, "__len__"):
-        if not isinstance(nside, torch.Tensor):
-            nside = torch.asarray(nside)
-        is_nside_ok = (
-            (nside == nside.int()) & (nside > 0) & (nside <= max_nside)
-        )
-        if nest:
-            is_nside_ok &= nside.int() & (nside.int() - 1 == 0)
-    else:
-        is_nside_ok = (nside == int(nside)) and (0 < nside <= max_nside)
-        if nest:
-            is_nside_ok = is_nside_ok and (int(nside) & (int(nside) - 1)) == 0
-    return is_nside_ok
-
-
-def check_nside(nside, nest=False):
-    """Raises exception if nside is not valid"""
-    if not torch.all(isnsideok(nside, nest=nest)):
-        raise ValueError(
-            f"{nside} is not a valid nside parameter (must be a power of 2,\
-                less than 2**30)"
-        )
-
-
-def lonlat2thetaphi(lon, lat):
-    return PI / 2.0 - torch.deg2rad(lat), torch.deg2rad(lon)
-
-
-def check_theta_valid(theta):
-    """Raises exception if theta is not within 0 and pi"""
-    theta = torch.asarray(theta)
-    if not ((theta >= 0).all() and (theta <= PI + 1e-5).all()):
-        raise ValueError("THETA is out of range [0,pi]")
-
-
-def ang2pix(nside, theta, phi, nest=False, lonlat=False):
-    check_nside(nside, nest=nest)
-
-    if lonlat:
-        theta, phi = lonlat2thetaphi(theta, phi)
-    check_theta_valid(theta)
-    check_nside(nside, nest=nest)
-    # if nest:
-    #     return pixlib._ang2pix_nest(nside, theta, phi) # this is written in c
-    # else:
-    #     return pixlib._ang2pix_ring(nside, theta, phi) # this is written in c
 
 
 def get_sky_projection(ra, dec, dist, nside=32, min_samples_per_pix=15):
@@ -103,7 +36,7 @@ def get_sky_projection(ra, dec, dist, nside=32, min_samples_per_pix=15):
     num_samples = len(ra)
 
     # calculate number of samples in each pixel
-    NPIX = nside2npix(nside)
+    NPIX = mlhp.nside2npix(nside)
 
     device = theta.device
     NPIX_arange = torch.arange(NPIX, device=device)
@@ -118,7 +51,7 @@ def get_sky_projection(ra, dec, dist, nside=32, min_samples_per_pix=15):
     m = torch.zeros(NPIX, device=device)
     m[torch.isin(NPIX_arange, uniq)] = counts.to(m.dtype)
     post = m / num_samples
-    post /= nside2pixarea(nside)
+    post /= mlhp.nside2pixarea(nside)
 
     # compute distance ansatz for pixels containing
     # greater than a threshold number
@@ -149,7 +82,7 @@ def get_sky_projection(ra, dec, dist, nside=32, min_samples_per_pix=15):
         dist_norm, device=device, dtype=norm.dtype
     )
 
-    uniq_ipix = nest2uniq(nside, NPIX_arange)
+    uniq_ipix = mlhp.nest2uniq(nside, NPIX_arange)
 
     uniq_ipix = uniq_ipix.cpu().numpy()
     post = post.cpu().numpy() * u.sr
@@ -195,7 +128,7 @@ class Result(bilby.result.Result):
         # count number of pixels before hitting the pixel with injection
         # in the sorted array
         num_pix_before_injection = 1 + torch.argmax(sorted_idxs == true_ipix)
-        searched_area = num_pix_before_injection * nside2pixarea(
+        searched_area = num_pix_before_injection * mlhp.nside2pixarea(
             nside, degrees=True
         )
         return searched_area
