05950d59403ce5c405f544c57fd49e293473bdac
Ethan Jacob Marx <ethan.marx@ligo.org>
2025-02-14 06:13:56 -0800

    add ability to create plots of data during testing

--- amplfi/train/models/flow.py
+++ amplfi/train/models/flow.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
+from gwpy.plot import Plot
+from gwpy.timeseries import TimeSeries
 
 from ..architectures.flows import FlowArchitecture
 from ..testing import Result
@@ -33,16 +35,17 @@ class FlowModel(AmplfiModel):
         num_corner: int = 10,
         nside: int = 32,
         min_samples_per_pix: int = 15,
+        plot_data: bool = False,
         **kwargs,
     ) -> None:
 
         super().__init__(*args, **kwargs)
-        # construct our model
         self.model = arch
         self.samples_per_event = samples_per_event
         self.num_corner = num_corner
         self.nside = nside
         self.min_samples_per_pix = min_samples_per_pix
+        self.plot_data = plot_data
 
         # save our hyperparameters
         self.save_hyperparameters(ignore=["arch"])
@@ -120,13 +123,128 @@ class FlowModel(AmplfiModel):
         )
         return r
 
+    def plot(
+        self, strain: np.ndarray, asds: np.ndarray, result: bilby.result.Result
+    ):
+        """
+        Create various plots for debugging purposes
+        """
+        sample_rate = self.trainer.datamodule.hparams.sample_rate
+
+        strain_filename = self.outdir / f"{self.idx}_whitened.png"
+        spec_filename = self.outdir / f"{self.idx}_spectrogram.png"
+        asd_filename = self.outdir / f"{self.idx}_asd.png"
+        freq_data_filename = self.outdir / f"{self.idx}_whitened_frequency.png"
+
+        ifos = self.trainer.datamodule.hparams.ifos
+
+        # whitened time domain strain
+        plt.figure()
+        plt.title("Whitened Time Domain Strain")
+
+        # window data
+        # window = scipy.signal.get_window(("tukey", 0.1), strain.shape[-1])
+        # strain *= window[None]
+
+        for i, ifo in enumerate(ifos):
+            plt.plot(strain[i], label=ifo)
+
+        plt.legend()
+        plt.savefig(strain_filename)
+        plt.close()
+
+        # qscans
+        qscans = []
+
+        for i, ifo in enumerate(ifos):
+            ts = TimeSeries(
+                strain[i],
+                dt=1 / sample_rate,
+            )
+
+            spec = ts.q_transform(
+                logf=True,
+                whiten=False,
+                frange=(25, 200),
+                qrange=(4, 108),
+                outseg=(3.35, 3.6),
+            )
+            qscans.append(spec)
+
+        chirp_mass, mass_ratio = (
+            result.injection_parameters["chirp_mass"],
+            result.injection_parameters["mass_ratio"],
+        )
+        title = f"chirp_mass: {chirp_mass:2f}, mass_ratio: {mass_ratio:2f}"
+        plot = Plot(
+            *qscans,
+            figsize=(18, 5),
+            geometry=(1, 2),
+            yscale="log",
+            method="pcolormesh",
+            cmap="viridis",
+            title=title,
+        )
+
+        for i, ax in enumerate(plot.axes):
+            label = "" if i != 1 else "Normalized Energy"
+            plot.colorbar(ax=ax, label=label)
+
+        plot.savefig(spec_filename)
+        plt.close()
+
+        # asds
+        frequencies = self.trainer.datamodule.frequencies.numpy()
+        mask = frequencies > self.trainer.datamodule.hparams.highpass
+        frequencies_masked = frequencies[mask]
+        plt.figure()
+        for i, ifo in enumerate(ifos):
+
+            plt.loglog(frequencies_masked, asds[i], label=f"{ifo} asd")
+            plt.title("ASDs")
+            plt.xlabel("Frequency (Hz)")
+            plt.ylabel("Scaled Amplitude")
+
+        plt.legend()
+        plt.savefig(asd_filename)
+        plt.close()
+
+        # whitened frequency domain data
+        fig, axes = plt.subplots(1, 2, figsize=(14, 6))
+        for i, ifo in enumerate(ifos):
+            strain_fft = np.fft.rfft(strain[i])
+            freqs = np.fft.rfftfreq(n=strain[i].shape[-1], d=1 / sample_rate)
+
+            axes[0].plot(
+                freqs,
+                strain_fft.real / (sample_rate ** (1 / 2)),
+                label=f"{ifo} data",
+            )
+            axes[1].plot(
+                freqs,
+                strain_fft.imag / (sample_rate ** (1 / 2)),
+                label=f"{ifo} data",
+            )
+
+        axes[0].set_title("Real")
+        axes[1].set_title("Imaginary")
+
+        axes[0].set_ylabel("Whitened Amplitude")
+        axes[0].set_xlabel("Frequency (Hz)")
+        axes[0].set_xlabel("Frequency (Hz)")
+
+        plt.legend()
+
+        plt.savefig(freq_data_filename)
+        plt.close()
+
     def on_test_epoch_start(self):
         self.test_results: list[Result] = []
         self.idx = 0
 
     def test_step(self, batch, _):
         strain, asds, parameters = batch
-        context = (strain, asds)
+        context = (strain, asds.clone())
 
         samples = self.model.sample(
             self.hparams.samples_per_event, context=context
