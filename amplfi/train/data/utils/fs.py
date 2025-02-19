5624bd54ca98b4f79f30664254998b45828b720a
Ethan Jacob Marx <ethan.marx@ligo.org>
2025-02-19 05:11:57 -0800

    make file download logging statements debug

--- amplfi/train/data/utils/fs.py
+++ amplfi/train/data/utils/fs.py
@@ -66,11 +66,11 @@ def _download(
     """
 
     lockfile = target + ".lock"
-    logging.info(f"Downloading {source} to {target}")
+    logging.debug(f"Downloading {source} to {target}")
     for i in range(num_retries):
         with FileLock(lockfile):
             if os.path.exists(target):
-                logging.info(
+                logging.debug(
                     f"Object {source} already downloaded by another process"
                 )
                 return
@@ -78,7 +78,7 @@ def _download(
                 s3.get(source, target)
                 break
             except (ResponseStreamingError, FSTimeoutError, ClientError):
-                logging.info(
+                logging.debug(
                     "Download attempt {} for object {} "
                     "was interrupted, retrying".format(i + 1, source)
                 )
