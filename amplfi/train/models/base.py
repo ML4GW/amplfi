a69ce23081abed11a46f819184c93d1e9428ef50
Ethan Jacob Marx <ethan.marx@ligo.org>
2025-04-03 12:11:30 -0700

    move to one cycle scheduler

--- amplfi/train/models/base.py
+++ amplfi/train/models/base.py
@@ -31,16 +31,13 @@ class AmplfiModel(pl.LightningModule):
         self,
         inference_params: list[str],
         learning_rate: float,
+        pct_start: float,
         train_outdir: Path,
         test_outdir: Optional[Path] = None,
         weight_decay: float = 0.0,
-        patience: int = 10,
-        factor: float = 0.1,
         verbose: bool = False,
     ):
         super().__init__()
-        self.scheduler_patience = patience
-        self.scheduler_factor = factor
         self._logger = self.init_logging(verbose)
 
         if test_outdir is None:
@@ -99,8 +96,9 @@ class AmplfiModel(pl.LightningModule):
             world_size = torch.distributed.get_world_size()
 
         lr = self.hparams.learning_rate * math.sqrt(world_size)
+
         optimizer = torch.optim.AdamW(
-            self.parameters(),
+            self.model.parameters(),
             lr=lr,
             weight_decay=self.hparams.weight_decay,
         )
@@ -108,20 +106,17 @@ class AmplfiModel(pl.LightningModule):
         scheduler = torch.optim.lr_scheduler.OneCycleLR(
             optimizer,
             max_lr=lr,
-            pct_start=0.2,
+            pct_start=self.hparams.pct_start,
             total_steps=self.trainer.estimated_stepping_batches,
         )
-        """
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer,
-            patience=self.scheduler_patience,
-            factor=self.scheduler_factor,
-        )
-        """
 
         return {
             "optimizer": optimizer,
-            "lr_scheduler": {"scheduler": scheduler, "monitor": "valid_loss"},
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "monitor": "valid_loss",
+                "interval": "step",
+            },
         }
 
     def scale(self, parameters, reverse: bool = False):
