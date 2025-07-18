use correct precision for gpstime

amplfi/train/data/datasets/base.py
@@ -746,4 +746,4 @@ class AmplfiDataset(pl.LightningDataModule):
                 strain = np.stack(strain, axis=0)
                 background.append(strain)
         background = np.stack(background, axis=0)
-        return torch.tensor(background), torch.tensor(analyzed_gpstimes)
+        return torch.tensor(background), analyzed_gpstimes

amplfi/train/data/datasets/testing.py
@@ -449,7 +449,7 @@ class RawStrainTestingDataset(FlowDataset):
 
     def predict_dataloader(self) -> torch.utils.data.DataLoader:
         dataset = torch.utils.data.TensorDataset(
-            self.background, torch.tensor(self.gpstimes)
+            self.background, torch.tensor(self.gpstimes, dtype=torch.float64)
         )
         dataloader = torch.utils.data.DataLoader(
             dataset,