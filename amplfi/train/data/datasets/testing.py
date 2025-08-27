a3bb8baf09188b0e1b819b43820111e04ee1f8df
Ethan Jacob Marx <ethan.marx@ligo.org>
2025-08-27 11:45:10 -0700

    clean up testing parameter handling

--- amplfi/train/data/datasets/testing.py
+++ amplfi/train/data/datasets/testing.py
@@ -75,7 +75,7 @@ class StrainTestingDataset(FlowDataset):
                 "StrainTestingDataset should only be used for testing"
             )
 
-        parameters = pd.read_hdf(self.dataset_path, key="parameters")[:4]
+        parameters = pd.read_hdf(self.dataset_path, key="parameters")
         # store dataframe indices to later save with outputs;
         # helpful for, e.g., cross-referencing with an injection set
         # that has been filtered so that indices are not just 0...N
@@ -95,7 +95,7 @@ class StrainTestingDataset(FlowDataset):
         with h5py.File(self.dataset_path, "r") as f:
             for ifo in self.hparams.ifos:
                 strain.append(
-                    torch.tensor(f["strain"][ifo][:4], dtype=torch.float32)
+                    torch.tensor(f["strain"][ifo], dtype=torch.float32)
                 )
         strain = torch.stack(strain, dim=1)
 
@@ -240,6 +240,7 @@ class ParameterTestingDataset(FlowDataset):
             )
 
         parameters = pd.read_hdf(self.dataset_path, key="parameters")[:4]
+        index = parameters.index
 
         load_keys = self.hparams.inference_params + [
             "ra",
@@ -288,7 +289,7 @@ class ParameterTestingDataset(FlowDataset):
         self.test_inference_params = torch.column_stack(params)
 
         # the whole set of parameters used to generate the waveforms
-        self.test_parameters: dict[str, torch.tensor] = parameters
+        self.test_parameters = pd.DataFrame(parameters, index=index)
         self.test_waveforms = torch.stack([cross, plus], dim=0)
 
         self.background, _ = self.background_from_gpstimes(
