971159d1eae78f720a1a1fbf14d01d0ce15ff6f0
Deep Chatterjee <deep.chatterjee@ligo.org>
2025-12-18 14:49:26 -0500

    noiseless views feature for SSL datasets (#276)
    * bugfixes and noiseless views feature for SSL datasets
    
    * lint and augment before zero noise view
    
    * include pytorch-tcn for tcn-based embeddings
    
    * Apply suggestions from code review

--- amplfi/train/data/datasets/similarity.py
+++ amplfi/train/data/datasets/similarity.py
@@ -1,25 +1,79 @@
+from typing import Optional
 import torch
 
-from .base import AmplfiDataset
+from .base import AmplfiDataset, Tensor
 
 
 class SimilarityDataset(AmplfiDataset):
     """
-    Lightning DataModule for training similarity networks
+    Lightning DataModule that returns two views of the underlying signal
+    decided by the augmentor. The augmentor is applied
+    to the signal (waveform), and maybe be injected to the
+    background. This is useful for
+    self-supervised learning methods.
 
     Args:
         augmentor:
             A torch module that transforms waveforms according to some
             symmetry that is meant to be marginalized over.
+        noiseless_view:
+            If True, the augmentor is applied to the waveform first.
+            For the reference view, this is then injected into background.
+            The augmented view is just the augmented waveform alone.
+            However, the augmented signal is still whitened with the same
+            PSD as the reference view.
     """
 
-    def __init__(self, *args, augmentor: torch.nn.Module, **kwargs):
+    def __init__(
+        self,
+        *args,
+        augmentor: torch.nn.Module,
+        noiseless_view: Optional[bool] = False,
+        **kwargs,
+    ):
         super().__init__(*args, **kwargs)
+        self.noiseless_view = noiseless_view
         self.augmentor = augmentor
+
+    def on_after_batch_transfer(
+        self, batch, _
+    ) -> tuple[Tensor, Tensor, dict[str, Tensor], Tensor]:
+        if self.trainer.training:
+            [batch] = batch
+            cross, plus, parameters = self.waveform_sampler.sample(batch)
+            [X_ref, X_aug], asds, parameters = self.inject(
+                batch, cross, plus, parameters
+            )
+
+        elif self.trainer.validating or self.trainer.sanity_checking:
+            [cross, plus, parameters], [background] = batch
+            background = background[: len(cross)]
+            keys = [
+                k
+                for k in self.hparams.inference_params
+                if k not in ["dec", "psi", "phi"]
+            ]
+            parameters = {k: parameters[:, i] for i, k in enumerate(keys)}
+            [X_ref, X_aug], asds, parameters = self.inject(
+                background, cross, plus, parameters
+            )
+
+        elif self.trainer.testing:
+            [cross, plus, parameters], [background] = batch
+            keys = [
+                k
+                for k in self.hparams.inference_params
+                if k not in ["dec", "psi", "phi"]
+            ]
+            parameters = {k: parameters[:, i] for i, k in enumerate(keys)}
+            [X_ref, X_aug], asds, parameters = self.inject(
+                background, cross, plus, parameters
+            )
+        return (X_ref, X_aug), asds, parameters
 
     def inject(self, X, cross, plus, parameters):
         X, psds = self.psd_estimator(X)
-        dec, psi, phi = self.waveform_sampler.sample_extrinsic(X)
+        dec, psi, phi = self.sample_extrinsic(X)
 
         waveforms = self.projector(dec, psi, phi, cross=cross, plus=plus)
         augmented = self.augmentor(waveforms)
@@ -40,8 +94,12 @@ class SimilarityDataset(AmplfiDataset):
             torch.Tensor(parameters[k]) for k in self.hparams.inference_params
         ]
         parameters = torch.vstack(parameters).T
-        X_ref = X + waveforms
-        X_aug = X + augmented
+        if self.noiseless_view:
+            X_ref = X + augmented
+            X_aug = augmented
+        else:
+            X_ref = X + waveforms
+            X_aug = X + augmented
         X_ref = self.whitener(X_ref, psds)
         X_aug = self.whitener(X_aug, psds)
 
